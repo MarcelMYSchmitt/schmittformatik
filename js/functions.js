a1fce305c1ca57f0f6342807a88ca7afd43616bc
mrmyiagi <marcel.mj.schmitt@web.de>
2019-04-14 22:09:05 +0200

    refactor usage of code

--- js/functions.js
+++ js/functions.js
@@ -14,9 +14,3 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
-
-
-var frmvalidator  = new Validator("contactform");
-frmvalidator.addValidation("name","req","Please provide your name");
-frmvalidator.addValidation("email","req","Please provide your email");
-frmvalidator.addValidation("email","email", "Please enter a valid email address");
